4c365376e251a76364920284c403021db71e103a
Sonia <sonia.bejar017@gmail.com>
2020-06-06 19:13:56 +0200

    Change PORT

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
     res.send('Welcome to Holy Key Universe');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server on port 3000");
 });
